c60cfcad063e48a207700904a85e77375539711d
Vilimas Vaitulevičius <s062523@ad.viko.lt>
2026-03-03 12:47:05 +0200

    Pabaigiau daryti visa koda ir prirasiau komentarus

--- main.cpp
+++ main.cpp
@@ -1,28 +1,40 @@
-#include <iostream>
-#include <iomanip>
-using namespace std;
+#include <iostream>     // Leidžia naudoti cout ir cin (įvestis/išvestis)
+#include <iomanip>      // Leidžia formatuoti skaičius, pvz. setprecision()
+using namespace std;    // Kad nereikėtų rašyti std::cout, std::cin
 
 int main() {
+    // Kintamieji vartotojo pasirinkimams ir skaičiavimams
     int pasirinkimas, valiuta;
     double suma, apskaiciuotaValiuta;
+
+    // Valiutų kursai: bendras, pirkimo ir pardavimo
     float GBP_Bendras = 0.8729;
     float GBP_Pirkti = 0.8600;
     float GBP_Parduoti = 0.9220;
+
     float USD_Bendras = 1.1793;
     float USD_Pirkti = 1.1460;
     float USD_Parduoti = 1.2340;
+
     float INR_Bendras = 104.6918;
     float INR_Pirkti = 101.3862;
     float INR_Parduoti = 107.8546;
 
+    // Nustatome, kad visi skaičiai būtų rodomi su 2 skaičiais po kablelio
     cout << fixed << setprecision(2);
 
-    cout << "1 - Palyginti valiuta" << endl;
-    cout << "2 - Pirkti valiuta" << endl;
-    cout << "3 - Parduoti valiuta" << endl;
+    // -----------------------------
+    //        PAGRINDINIS MENIU
+    // -----------------------------
+    cout << "1 - Palyginti valiuta" << endl;   // Rodo bendrą kursą
+    cout << "2 - Pirkti valiuta" << endl;      // EUR -> kita valiuta
+    cout << "3 - Parduoti valiuta" << endl;    // Kita valiuta -> EUR
     cout << "Pasirinkimas: ";
-    cin >> pasirinkimas;
+    cin >> pasirinkimas;                       // Vartotojas įveda pasirinkimą
 
+    // -----------------------------
+    //       VALIUTOS PASIRINKIMAS
+    // -----------------------------
     cout <<"Pasirinkite valiuta:" << endl;
     cout << "1 - GBP" << endl;
     cout << "2 - USD" << endl;
@@ -30,6 +42,9 @@ int main() {
     cout << "Jusu pasirinkimas: ";
     cin >> valiuta;
 
+    // -----------------------------
+    // 1. PALYGINIMAS (rodyti kursą)
+    // -----------------------------
     if (pasirinkimas == 1) {
 
         if (valiuta == 1)
@@ -42,11 +57,16 @@ int main() {
             cout << "Neteisingas valiutos pasirinkimas" << endl;
 
     }
+
+    // -----------------------------
+    // 2. PIRKIMAS (EUR -> valiuta)
+    // -----------------------------
     else if (pasirinkimas == 2) {
 
         cout << "Iveskite suma eurais: ";
-        cin >> suma;
+        cin >> suma;   // Vartotojas įveda kiek EUR nori iškeisti
 
+        // Pagal pasirinktą valiutą dauginame iš pirkimo kurso
         if (valiuta == 1)
             apskaiciuotaValiuta = suma * GBP_Pirkti;
         else if (valiuta == 2)
@@ -55,16 +75,21 @@ int main() {
             apskaiciuotaValiuta = suma * INR_Pirkti;
         else {
             cout << "Neteisingas valiutos pasirinkimas" << endl;
-            return 0;
+            return 0; // Baigia programą, jei įvesta bloga valiuta
         }
 
         cout << "Jusu nauja valiuta: " << apskaiciuotaValiuta << endl;
     }
+
+    // -----------------------------
+    // 3. PARDAVIMAS (valiuta -> EUR)
+    // -----------------------------
     else if (pasirinkimas == 3) {
 
         cout << "Iveskite valiutos suma: ";
-        cin >> suma;
+        cin >> suma;  // Vartotojas įveda kiek turi valiutos
 
+        // Daliname iš pardavimo kurso, kad gautume EUR
         if (valiuta == 1)
             apskaiciuotaValiuta = suma / GBP_Parduoti;
         else if (valiuta == 2)
@@ -78,9 +103,13 @@ int main() {
 
         cout << "Jusu nauja valiuta: " << apskaiciuotaValiuta << " EUR" << endl;
     }
+
+    // -----------------------------
+    // NETEISINGAS MENIU PASIRINKIMAS
+    // -----------------------------
     else {
         cout << "Neteisingas pasirinkimas" << endl;
     }
 
-    return 0;
+    return 0; // Programos pabaiga
 }
