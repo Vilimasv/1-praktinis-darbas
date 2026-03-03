b61b7ec364180bc64aa37f33e8c565fd62b3c428
Vilimas Vaitulevičius <s062523@ad.viko.lt>
2026-03-03 13:44:56 +0200

    pridejau while cikla, kad kodas kartotusi

--- main.cpp
+++ main.cpp
@@ -3,113 +3,121 @@
 using namespace std;    // Kad nereikėtų rašyti std::cout, std::cin
 
 int main() {
-    // Kintamieji vartotojo pasirinkimams ir skaičiavimams
-    int pasirinkimas, valiuta;
-    double suma, apskaiciuotaValiuta;
+    bool programIsRunning = true;
+    while (programIsRunning) {
+        // Kintamieji vartotojo pasirinkimams ir skaičiavimams
+        int pasirinkimas, valiuta;
+        double suma, apskaiciuotaValiuta;
 
-    // Valiutų kursai: bendras, pirkimo ir pardavimo
-    float GBP_Bendras = 0.8729;
-    float GBP_Pirkti = 0.8600;
-    float GBP_Parduoti = 0.9220;
+        // Valiutų kursai: bendras, pirkimo ir pardavimo
+        float GBP_Bendras = 0.8729;
+        float GBP_Pirkti = 0.8600;
+        float GBP_Parduoti = 0.9220;
 
-    float USD_Bendras = 1.1793;
-    float USD_Pirkti = 1.1460;
-    float USD_Parduoti = 1.2340;
+        float USD_Bendras = 1.1793;
+        float USD_Pirkti = 1.1460;
+        float USD_Parduoti = 1.2340;
 
-    float INR_Bendras = 104.6918;
-    float INR_Pirkti = 101.3862;
-    float INR_Parduoti = 107.8546;
+        float INR_Bendras = 104.6918;
+        float INR_Pirkti = 101.3862;
+        float INR_Parduoti = 107.8546;
 
-    // Nustatome, kad visi skaičiai būtų rodomi su 2 skaičiais po kablelio
-    cout << fixed << setprecision(2);
+        // Nustatome, kad visi skaičiai būtų rodomi su 2 skaičiais po kablelio
+        cout << fixed << setprecision(2);
 
-    // -----------------------------
-    //        PAGRINDINIS MENIU
-    // -----------------------------
-    cout << "1 - Palyginti valiuta" << endl;   // Rodo bendrą kursą
-    cout << "2 - Pirkti valiuta" << endl;      // EUR -> kita valiuta
-    cout << "3 - Parduoti valiuta" << endl;    // Kita valiuta -> EUR
-    cout << "Pasirinkimas: ";
-    cin >> pasirinkimas;                       // Vartotojas įveda pasirinkimą
+        // -----------------------------
+        //        PAGRINDINIS MENIU
+        // -----------------------------
+        cout << "1 - Palyginti valiuta" << endl;   // Rodo bendrą kursą
+        cout << "2 - Pirkti valiuta" << endl;      // EUR -> kita valiuta
+        cout << "3 - Parduoti valiuta" << endl;    // Kita valiuta -> EUR
+        cout << "4 - Iseiti" <<endl;
+        cout << "Pasirinkimas: ";
+        cin >> pasirinkimas;    // Vartotojas įveda pasirinkimą
 
-    // -----------------------------
-    //       VALIUTOS PASIRINKIMAS
-    // -----------------------------
-    cout <<"Pasirinkite valiuta:" << endl;
-    cout << "1 - GBP" << endl;
-    cout << "2 - USD" << endl;
-    cout << "3 - INR" << endl;
-    cout << "Jusu pasirinkimas: ";
-    cin >> valiuta;
-
-    // -----------------------------
-    // 1. PALYGINIMAS (rodyti kursą)
-    // -----------------------------
-    if (pasirinkimas == 1) {
-
-        if (valiuta == 1)
-            cout << "1 EUR = " << GBP_Bendras << " GBP" << endl;
-        else if (valiuta == 2)
-            cout << "1 EUR = " << USD_Bendras << " USD" << endl;
-        else if (valiuta == 3)
-            cout << "1 EUR = " << INR_Bendras << " INR" << endl;
-        else
-            cout << "Neteisingas valiutos pasirinkimas" << endl;
-
-    }
-
-    // -----------------------------
-    // 2. PIRKIMAS (EUR -> valiuta)
-    // -----------------------------
-    else if (pasirinkimas == 2) {
-
-        cout << "Iveskite suma eurais: ";
-        cin >> suma;   // Vartotojas įveda kiek EUR nori iškeisti
-
-        // Pagal pasirinktą valiutą dauginame iš pirkimo kurso
-        if (valiuta == 1)
-            apskaiciuotaValiuta = suma * GBP_Pirkti;
-        else if (valiuta == 2)
-            apskaiciuotaValiuta = suma * USD_Pirkti;
-        else if (valiuta == 3)
-            apskaiciuotaValiuta = suma * INR_Pirkti;
-        else {
-            cout << "Neteisingas valiutos pasirinkimas" << endl;
-            return 0; // Baigia programą, jei įvesta bloga valiuta
+        if (pasirinkimas == 4) {
+            programIsRunning = false;
+            break;
         }
 
-        cout << "Jusu nauja valiuta: " << apskaiciuotaValiuta << endl;
-    }
+        // -----------------------------
+        //       VALIUTOS PASIRINKIMAS
+        // -----------------------------
+        cout <<"Pasirinkite valiuta:" << endl;
+        cout << "1 - GBP" << endl;
+        cout << "2 - USD" << endl;
+        cout << "3 - INR" << endl;
+        cout << "Jusu pasirinkimas: ";
+        cin >> valiuta;
 
-    // -----------------------------
-    // 3. PARDAVIMAS (valiuta -> EUR)
-    // -----------------------------
-    else if (pasirinkimas == 3) {
+        // -----------------------------
+        // 1. PALYGINIMAS (rodyti kursą)
+        // -----------------------------
+        if (pasirinkimas == 1) {
 
-        cout << "Iveskite valiutos suma: ";
-        cin >> suma;  // Vartotojas įveda kiek turi valiutos
+            if (valiuta == 1)
+                cout << "1 EUR = " << GBP_Bendras << " GBP" << endl;
+            else if (valiuta == 2)
+                cout << "1 EUR = " << USD_Bendras << " USD" << endl;
+            else if (valiuta == 3)
+                cout << "1 EUR = " << INR_Bendras << " INR" << endl;
+            else
+                cout << "Neteisingas valiutos pasirinkimas" << endl;
 
-        // Daliname iš pardavimo kurso, kad gautume EUR
-        if (valiuta == 1)
-            apskaiciuotaValiuta = suma / GBP_Parduoti;
-        else if (valiuta == 2)
-            apskaiciuotaValiuta = suma / USD_Parduoti;
-        else if (valiuta == 3)
-            apskaiciuotaValiuta = suma / INR_Parduoti;
-        else {
-            cout << "Neteisingas valiutos pasirinkimas" << endl;
-            return 0;
         }
 
-        cout << "Jusu nauja valiuta: " << apskaiciuotaValiuta << " EUR" << endl;
-    }
+        // -----------------------------
+        // 2. PIRKIMAS (EUR -> valiuta)
+        // -----------------------------
+        else if (pasirinkimas == 2) {
 
-    // -----------------------------
-    // NETEISINGAS MENIU PASIRINKIMAS
-    // -----------------------------
-    else {
-        cout << "Neteisingas pasirinkimas" << endl;
-    }
+            cout << "Iveskite suma eurais: ";
+            cin >> suma;   // Vartotojas įveda kiek EUR nori iškeisti
 
+            // Pagal pasirinktą valiutą dauginame iš pirkimo kurso
+            if (valiuta == 1)
+                apskaiciuotaValiuta = suma * GBP_Pirkti;
+            else if (valiuta == 2)
+                apskaiciuotaValiuta = suma * USD_Pirkti;
+            else if (valiuta == 3)
+                apskaiciuotaValiuta = suma * INR_Pirkti;
+            else {
+                cout << "Neteisingas valiutos pasirinkimas" << endl;
+                return 0; // Baigia programą, jei įvesta bloga valiuta
+            }
+
+            cout << "Jusu nauja valiuta: " << apskaiciuotaValiuta << endl;
+        }
+
+        // -----------------------------
+        // 3. PARDAVIMAS (valiuta -> EUR)
+        // -----------------------------
+        else if (pasirinkimas == 3) {
+
+            cout << "Iveskite valiutos suma: ";
+            cin >> suma;  // Vartotojas įveda kiek turi valiutos
+
+            // Daliname iš pardavimo kurso, kad gautume EUR
+            if (valiuta == 1)
+                apskaiciuotaValiuta = suma / GBP_Parduoti;
+            else if (valiuta == 2)
+                apskaiciuotaValiuta = suma / USD_Parduoti;
+            else if (valiuta == 3)
+                apskaiciuotaValiuta = suma / INR_Parduoti;
+            else {
+                cout << "Neteisingas valiutos pasirinkimas" << endl;
+                return 0;
+            }
+
+            cout << "Jusu nauja valiuta: " << apskaiciuotaValiuta << " EUR" << endl;
+        }
+
+        // -----------------------------
+        // NETEISINGAS MENIU PASIRINKIMAS
+        // -----------------------------
+        else {
+            cout << "Neteisingas pasirinkimas" << endl;
+        }
+    }
     return 0; // Programos pabaiga
 }
